a3a63467862af3b733b3134ff511bb8d376ec781
Aayush Tyagi <ayushtyagi@gmail.com>
2019-01-18 18:53:23 -0800

    oof this is so long

--- app/src/main/java/org/mobiledevsberkeley/basicsnapchatclone/MainActivity.java
+++ app/src/main/java/org/mobiledevsberkeley/basicsnapchatclone/MainActivity.java
@@ -2,6 +2,9 @@ package org.mobiledevsberkeley.basicsnapchatclone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText editText = findViewById(R.id.editText);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(getString(R.string.default_text));
+            }
+        });
     }
 }
