4b05b2e47e2c0053f3058b9d710401b946e2d33d
Aayush Tyagi <ayushtyagi@gmail.com>
2019-02-09 16:54:38 -0800

    finished the thing

--- app/src/main/java/org/mobiledevsberkeley/basicsnapchatclone/MainActivity.java
+++ app/src/main/java/org/mobiledevsberkeley/basicsnapchatclone/MainActivity.java
@@ -1,13 +1,20 @@
 package org.mobiledevsberkeley.basicsnapchatclone;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int CAMERA_REQUEST_ID = 42;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +25,8 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                editText.setText(getString(R.string.default_text));
+                Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(takePhotoIntent, CAMERA_REQUEST_ID);
             }
         });
     }
@@ -27,5 +35,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         Toast.makeText(this, "activity paused!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "activity resumed!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == CAMERA_REQUEST_ID && resultCode == RESULT_OK) {
+            Intent intent = new Intent(MainActivity.this, ViewActivity.class);
+            EditText editText = findViewById(R.id.editText);
+            intent.putExtra("text", editText.getText().toString());
+            intent.putExtra("image", (Bitmap) data.getExtras().get("data"));
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/org/mobiledevsberkeley/basicsnapchatclone/ViewActivity.java
+++ app/src/main/java/org/mobiledevsberkeley/basicsnapchatclone/ViewActivity.java
@@ -1,7 +1,10 @@
 package org.mobiledevsberkeley.basicsnapchatclone;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ViewActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class ViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
+
+        String text = getIntent().getStringExtra("text");
+
+        // a bitmap extends the parcelable class, and java 8 will auto cast to bitmap in this line
+        Bitmap bitmap = getIntent().getParcelableExtra("image");
+
+        TextView caption = findViewById(R.id.caption);
+        ImageView imageView = findViewById(R.id.imageView);
+
+        imageView.setImageBitmap(bitmap);
+        caption.setText(text);
     }
 }
